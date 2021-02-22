be2bae666d0d34fdff5b5ba43b91084ebdb2cbab
marcuspramirez <@macusramirez@gmail.com>
2021-02-21 21:32:57 -0600

    about-page

---
+++ src/components/AboutCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './AboutCard.css';
+import CardItem from './CardItem';
+
+function AboutCard() {
+    return (
+        <div className='cards'>
+            <h1>Brittany A. Moncivais is a 5x Certified Makeup Artist and has been in the beauty industry for the last 12 years. Brittany's is a certified pro with certifications from MAC, Makeup Forever, Mario Dedivanovic (2x), and Temptu Airbrush. Brittany and her team currently specialize in Bridal Hair and Makeup. Our team provides In-Studio and On-Location hair and makeup services.</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/brit-about.jpg'
+                            text='Let Brittany Help You Take Your Grooms Breath away'
+                            label='Brit'
+                            path='/services'
+                        />
+
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AboutCard;
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import '../../App.css';
+import AboutCard from '../AboutCard';
 
-export default function About() {
-  return <h1 className='about'>Brittany A. Moncivais is a 5x Certified Makeup Artist and has been in the beauty industry for the last 12 years. Brittany's is a certified pro with certifications from MAC, Makeup Forever, Mario Dedivanovic (2x), and Temptu Airbrush. Brittany and her team currently specialize in Bridal Hair and Makeup. Our team provides In-Studio and On-Location hair and makeup services.</h1>;
+
+
+function About() {
+  return (
+    <>
+     
+      <AboutCard />
+      
+    </>
+  );
 }
+
+export default About;
