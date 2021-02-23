33dfaa5882790bd338b629e2212ce28f6273caab
marcuspramirez <@macusramirez@gmail.com>
2021-02-23 16:00:15 -0600

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Button } from './Button'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './Footer.css';
 
 function Footer() {
     return (
         <div className='footer-container'>
             <section className="footer-subscription">
                 <p className="footer-subscription-heading">
-                    Call Beauty by Brittany to receive our best cosmetic deals
+                    Subscibe to get the latest emails and deals 
                 </p>
                 <p className="footer-subscription-text">
                     You can unsubscribe at any time
@@ -20,6 +22,81 @@ function Footer() {
                         />
                         <Button buttonStyle='btn--outline'>Subscibe</Button>
                     </form>
+                </div>
+            </section>
+            <div className="footer-links">
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>Contact</h2>
+                        <Link to='/'>Call</Link>
+                        <Link to='/'>Email</Link>
+                        
+                    </div>
+                </div>
+                <div className="footer-link-wrapper">
+                    <div className="footer-link-items">
+                        <h2>About Us</h2>
+                        <Link to='/sign-up'>How it works</Link>
+                        <Link to='/'>Testimonials</Link>
+                        <Link to='/'>Careers</Link>
+                        <Link to='/'>Investors</Link>
+                        <Link to='/'>Terms of Service</Link>
+                    </div>
+                    <div className="footer-link-items">
+                        <h2>Social Media</h2>
+                        <Link to='/'>Instagram</Link>
+                        <Link to='/'>Facebook</Link>
+                        <Link to='/'>Youtube</Link>
+                        <Link to='/'>Twitter</Link>
+                    </div>
+                </div>
+            </div>
+            <section className="social-media">
+                <div className="social-media-wrap">
+                    <div className="footer-logo">
+                        <Link to="/" className="social-logo">
+                            B3 <i className="fab fa-typo3"></i>
+                        </Link>
+                    </div>
+                    <small className="website-rights">BBB 2020</small>
+                    <div className="social-icons">
+                        <Link className="social-icon-link facebook"
+                            to="/"
+                            target="_blank"
+                            arial-label="Facebook"
+                        >
+                            <i className="fab fa-facebook-f"></i>
+                        </Link>
+                        <Link className="social-icon-link instagram"
+                            to="/"
+                            target="_blank"
+                            arial-label="Instagram"
+                        >
+                            <i className="fab fa-instagram"></i>
+                        </Link>
+                        <Link className="social-icon-link twitter"
+                            to="/"
+                            target="_blank"
+                            arial-label="Twitter"
+                        >
+                            <i className="fab fa-twitter"></i>
+                        </Link>
+                        <Link className="social-icon-link linkedin"
+                            to="/"
+                            target="_blank"
+                            arial-label="LinkedIn"
+                        >
+                            <i className="fab fa-linkedin"></i>
+                        </Link>
+                    </div>
                 </div>
             </section>
         </div>
